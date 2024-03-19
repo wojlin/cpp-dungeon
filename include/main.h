36b574e72d5531e12e32bad3b0cc1041f7abd087
wojlin <wojciech.linowski.2137@gmail.com>
2024-03-19 10:48:40 +0100

    code refactor 2

--- include/main.h
+++ include/main.h
@@ -33,142 +33,22 @@
 #define NETWORK_PORT 2137
 #define NETWORK_ERROR 69
 
-
-class core
+class debuggingInstance
 {
     public:
-
-       int launch(int argc, char* argv[])
+        void launch()
         {
-            bool end = false;
-
-            if(argc == 1)
-            {
-                std::cout << "### " << GAME_NAME << " debugging tool ###" << std::endl;
-                createServer(NETWORK_PORT);
-                launchGameInstance();
-                awaitConnection();
-                awaitMessages();
-            }
-            else
-            {
-                
-                createClient();
-                setupGUI();
-                
-                
-                levelGenerator level(1);
-                level.generatePreview("preview.png", 1);
-
-                bool end = false;
-                while(!end)
-                {
-                    level.print(level.getPlayerPos().x, level.getPlayerPos().y, 80, 40);
-                    int input = getch();
-                    clear();
-                    if(input == 68 || input == 97) 
-                    { 
-                        coords pos = level.getPlayerPos();
-                        level.setPlayerPos(pos.x - 1, pos.y);
-
-                        send(clientSocket, "A", strlen("A"), 0);
-            
-                    }
-                    else if(input == 67 || input == 100) 
-                    { 
-                        coords pos = level.getPlayerPos();
-                        level.setPlayerPos(pos.x + 1, pos.y);
-
-                        
-                        send(clientSocket, "D", strlen("D"), 0);
-                    }
-                    else if(input == 65 || input == 119) 
-                    { 
-                        coords pos = level.getPlayerPos();
-                        level.setPlayerPos(pos.x, pos.y - 1);
-
-                        
-                        send(clientSocket, "S", strlen("S"), 0);
-                    }
-                    else if (input == 66 || input == 115) 
-                    { 
-                        coords pos = level.getPlayerPos();
-                        level.setPlayerPos(pos.x, pos.y + 1);
-
-                        send(clientSocket, "W", strlen("W"), 0);
-                    }
-                    else if(input == 'q')
-                    {
-                        send(clientSocket, "end", strlen("end"), 0);
-                        end=true;
-                    }
-                    else 
-                    { 
-                        mvprintw(0, 0, "%d", input);
-                    }
-
-                }
-                
-                teardownGUI();
-            }
-            
-
-            
-            return 0;
+            std::cout << "### " << GAME_NAME << " debugging tool ###" << std::endl;
+            createServer(NETWORK_PORT);
+            launchGameInstance();
+            awaitConnection();
+            awaitMessages();
         }
 
     protected:
 
         int serverSocket = -1;
         int clientSocket = -1;
-
-        void teardownGUI()
-        {
-            endwin();   // End ncurses
-        }
-
-        void setupGUI()
-        {
-            std::locale::global(std::locale("")); // Set the global locale to the user's preferred locale
-            std::wcout.imbue(std::locale()); // Set the wide character output stream to use the global locale
-            setlocale(LC_ALL, "");
-
-            initscr();
-            start_color();
-            cbreak();
-            noecho();
-            //keypad(stdscr, TRUE);
-
-            // Enable Unicode support
-            if (!has_colors() || !can_change_color() || !has_ic()) {
-                endwin();
-                throw GUIError("failed to initialize curses module");
-            }
-            if (use_default_colors() == ERR) {
-                endwin();
-                throw GUIError("failed to initialize curses module");
-            }
-
-        }
-
-        void createClient()
-        {
-            clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-            if (clientSocket == -1) 
-            {
-                throw NetworkError("Error creating socket");
-            }
-
-            sockaddr_in serverAddr;
-            serverAddr.sin_family = AF_INET;
-            serverAddr.sin_port = htons(NETWORK_PORT); 
-            inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-
-            if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) 
-            {  
-                throw NetworkError("Error connecting to server");
-            }
-        }
 
         void createServer(int port)
         {
@@ -251,7 +131,146 @@ class core
             close(clientSocket);
             close(serverSocket);
         }
+};
 
+class gameInstance
+{
+    public:
+
+        void launch()
+        {
+            createClient();
+            setupGUI();
+            
+            
+            levelGenerator level(1);
+            level.generatePreview("preview.png", 1);
+
+            bool end = false;
+            while(!end)
+            {
+                level.print(level.getPlayerPos().x, level.getPlayerPos().y, 80, 40);
+                int input = getch();
+                clear();
+                if(input == 68 || input == 97) 
+                { 
+                    coords pos = level.getPlayerPos();
+                    level.setPlayerPos(pos.x - 1, pos.y);
+
+                    send(clientSocket, "A", strlen("A"), 0);
+        
+                }
+                else if(input == 67 || input == 100) 
+                { 
+                    coords pos = level.getPlayerPos();
+                    level.setPlayerPos(pos.x + 1, pos.y);
+
+                    
+                    send(clientSocket, "D", strlen("D"), 0);
+                }
+                else if(input == 65 || input == 119) 
+                { 
+                    coords pos = level.getPlayerPos();
+                    level.setPlayerPos(pos.x, pos.y - 1);
+
+                    
+                    send(clientSocket, "S", strlen("S"), 0);
+                }
+                else if (input == 66 || input == 115) 
+                { 
+                    coords pos = level.getPlayerPos();
+                    level.setPlayerPos(pos.x, pos.y + 1);
+
+                    send(clientSocket, "W", strlen("W"), 0);
+                }
+                else if(input == 'q')
+                {
+                    send(clientSocket, "end", strlen("end"), 0);
+                    end=true;
+                }
+                else 
+                { 
+                    mvprintw(0, 0, "%d", input);
+                }
+
+            }
+            
+            teardownGUI();
+        }
+
+    protected:
+        int clientSocket = -1;
+
+        void setupGUI()
+        {
+            std::locale::global(std::locale("")); // Set the global locale to the user's preferred locale
+            std::wcout.imbue(std::locale()); // Set the wide character output stream to use the global locale
+            setlocale(LC_ALL, "");
+
+            initscr();
+            start_color();
+            cbreak();
+            noecho();
+            //keypad(stdscr, TRUE);
+
+            // Enable Unicode support
+            if (!has_colors() || !can_change_color() || !has_ic()) {
+                endwin();
+                throw GUIError("failed to initialize curses module");
+            }
+            if (use_default_colors() == ERR) {
+                endwin();
+                throw GUIError("failed to initialize curses module");
+            }
+
+        }
+
+        void createClient()
+        {
+            clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+            if (clientSocket == -1) 
+            {
+                throw NetworkError("Error creating socket");
+            }
+
+            sockaddr_in serverAddr;
+            serverAddr.sin_family = AF_INET;
+            serverAddr.sin_port = htons(NETWORK_PORT); 
+            inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+            if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) 
+            {  
+                throw NetworkError("Error connecting to server");
+            }
+        }
+
+        void teardownGUI()
+        {
+            endwin();   // End ncurses
+        }
+
+};
+
+class core
+{
+    public:
+
+        int launch(int argc, char* argv[])
+        {
+            if(argc == 1)
+            {
+                debuggingInstance debug = debuggingInstance();
+                debug.launch();
+            }
+            else
+            {
+                gameInstance game = gameInstance();
+                game.launch();
+            }
+            return 0;
+        }
+
+    protected:
         
 };
 
